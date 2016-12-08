87602d2e2f29955c0535d1a847fbe57a94a07d14
rand0me <not.randome@gmail.com>
2016-12-08 19:18:28 +0300

    fix(build): clean dependecies

--- tools/rewrite-published-package.js
+++ tools/rewrite-published-package.js
@@ -1,6 +1,5 @@
 /**
  * Rewrite the package.json that gets published to npm.
- * * Change main to point to angularfire.js instead of dist/angularfire.js
  * * Change angular2 to be a peer dependency
  */
 var fs = require('fs');
@@ -14,15 +13,6 @@ delete peerDependencies.firebase;
 
 var outPackage = Object.assign({}, srcPackage, {
   peerDependencies,
-  dependencies: {
-    /**
-     * Firebase SDK should be a dependency since it's not required that
-     * projects will install/use the SDK directly. And since Firebase uses
-     * semver, the version restriction is more permissive. This means if the user
-     * has installed Firebase already, npm is more likely to find a matching version.
-     **/
-     firebase: srcPackage.dependencies.firebase
-  }
 });
 
 fs.writeFileSync('./dist/package.json', JSON.stringify(outPackage, null, 2));
